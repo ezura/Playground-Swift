4bea6b19618c158ea05ebb924a03716c90fc4ea5
yuka ezura <yuka.ezura@gmail.com>
2017-04-09 15:58:31 +0900

    add feedback

--- TCM20170407.playground/Pages/zombie_var.xcplaygroundpage/Contents.swift
+++ TCM20170407.playground/Pages/zombie_var.xcplaygroundpage/Contents.swift
@@ -3,9 +3,11 @@
 import Foundation
 
 class A {
-    lazy var zombie: Int! = {
-        return 1
-    }()
+//    lazy var zombie: Int! = {
+//        return 1
+//    }()
+    
+    lazy var zombie: Int! = 1
 }
 
 let a = A()
@@ -16,5 +18,36 @@ a.zombie  // 1000000000
 
 a.zombie = nil
 a.zombie  // 1
+
+
+
+/**
+ lazy 繋がりで @trunon さんに教えてもらったコード
+ 
+ 初期化前にアクセスした場合、殺す
+ */
+
+class B {
+    var v1: Int!
+    
+    // こっちのが良い
+    lazy var v2: Int = {
+        fatalError("`\(#function)` is not initialized")
+    }()
+}
+
+let b = B()
+// fatal error: unexpectedly found nil while unwrapping an Optional value
+//b.v1.description
+
+// nil 入っちゃう
+b.v1 = nil
+
+
+// fatal error: `v2` is not initialized
+//b.v2.description
+
+// nil は入り得ない
+//b.v2 = nil
 
 //: [Next](@next)
